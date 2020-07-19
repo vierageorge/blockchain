9f71a2f6057dc2af3e35928a0dbeb2e82aaee9cb
Jorge Viera <joviera@gmail.com>
2020-07-19 11:23:41 -0500

    Adds capability to check if blockchain is valid

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -95,3 +95,14 @@ def get_chain():
         'length': len(blockchain.chain)
     }
     return jsonify(response), 200
+
+# Checking if the blockchain is valid
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    if blockchain.is_chain_valid(blockchain.chain):
+        return jsonify({'message': 'Yeiiii!! Blockchain is valid!'}), 200
+    else:
+        return jsonify({'message': 'Booo!! Blockchain is NOT valid!'}), 200
+
+# Running the web app
+app.run(host='0.0.0.0', port=5000)
