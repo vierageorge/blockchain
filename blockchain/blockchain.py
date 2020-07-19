cb54870577fd0730b7f1432e2bcdd0ae686d56b7
Jorge Viera <joviera@gmail.com>
2020-07-19 09:49:20 -0500

    Adds proof of work function

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -13,7 +13,7 @@ class Blockchain:
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0') # Genesis block
 
-    def create_block(self, proof, previous_hash):
+    def create_block(self, proof, previous_hash): 
         block = {
             'index': len(self.chain) + 1,
             'timestamp': str(datetime.datetime.now()),
@@ -22,5 +22,19 @@ class Blockchain:
         }
         self.chain.append(block)
         return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while not check_proof:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+            return new_proof
 
 # Mining the Blockchain
