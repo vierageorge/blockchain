ea58f6b4a91b22b34abfbcdf584be3616e7d7aea
Jorge Viera <joviera@gmail.com>
2020-07-19 08:26:37 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blockchain
+
+In this repo, I'll be placing all the files generated through my blockchain learning path.
---
+++ blockchain/blockchain.py
@@ -0,0 +1,11 @@
+# Create a Simple Blockchain.
+
+# Imports
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Building the Blockchain
+
+# Mining the Blockchain
