2ed50121632812133f21cbffd317827624d85a23
Jorge Viera <joviera@gmail.com>
2020-07-19 10:50:36 -0500

    Adds get complete chain capability to the web app

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -85,3 +85,13 @@ def mine_block():
         'previous_hash': block['previous_hash']
     }
     return jsonify(response), 200
+
+
+# Getting the complete blockchain
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
