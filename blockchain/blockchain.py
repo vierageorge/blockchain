396e72961020d958eaa119daf72c8810d83a725a
Jorge Viera <joviera@gmail.com>
2020-07-19 08:49:45 -0500

    Starts creating the blockchain, with create block method

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -8,4 +8,19 @@ from flask import Flask, jsonify
 
 # Building the Blockchain
 
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0') # Genesis block
+
+    def create_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'previous_hash': previous_hash
+        }
+        self.chain.append(block)
+        return block
+
 # Mining the Blockchain
